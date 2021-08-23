4f5de684641794d1c1411db648f556f2a1279091
tahrimhossain <tahrimh246@gmail.com>
2021-08-23 20:42:55 +0600

    fixed try catch in API.dart

--- lib/Services/API.dart
+++ lib/Services/API.dart
@@ -9,49 +9,42 @@ import 'dart:convert';
 class API{
 
   Future<CovidData> getCovidData()async{
-      try{
-        http.Response response = await http.get(Uri.parse('https://api.covid19api.com/summary'));
-        if(response.statusCode == 200){
-          return CovidData.fromJson(json.decode(response.body));
-        }
-        else{
-          throw Exception();
-        }
-      }catch(e){
-        throw Exception("Error fetching Covid Data");
-      }
+    http.Response response = await http.get(Uri.parse('https://api.covid19api.com/summary'));
+    if(response.statusCode == 200){
+      return CovidData.fromJson(json.decode(response.body));
+    }
+    else{
+      throw Exception("Error Fetching Data");
+    }
+
   }
 
   Future<List<FlSpot>> fetchConfirmedGraphData(String countryName)async
   {
 
-    try{
-      http.Response response = await http.get(Uri.parse("https://api.covid19api.com/total/dayone/country/"+countryName+"/status/confirmed"));
+    http.Response response = await http.get(Uri.parse("https://api.covid19api.com/total/dayone/country/"+countryName+"/status/confirmed"));
 
-      if(response.statusCode == 200){
+    if(response.statusCode == 200){
 
-        List<FlSpot> listOfSpots = <FlSpot>[];
+      List<FlSpot> listOfSpots = <FlSpot>[];
 
-        int count = 0;
+      int count = 0;
 
-        json.decode(response.body).forEach((element){
-          GraphData data = GraphData.fromJson(element);
-          int temp = data.count;
-          data.count -= count;
-          if(data.count < 0){
-            data.count = 0;
-          }
-          count = temp;
-          listOfSpots.add(FlSpot(data.date.millisecondsSinceEpoch.toDouble(), data.count.toDouble()));
-        });
-        
-        return listOfSpots;
+      json.decode(response.body).forEach((element){
+        GraphData data = GraphData.fromJson(element);
+        int temp = data.count;
+        data.count -= count;
+        if(data.count < 0){
+          data.count = 0;
+        }
+        count = temp;
+        listOfSpots.add(FlSpot(data.date.millisecondsSinceEpoch.toDouble(), data.count.toDouble()));
+      });
 
-      }else{
-        throw Exception();
-      }
-    }catch(e){
-        throw Exception("Error Fetching Data");
+      return listOfSpots;
+
+    }else{
+      throw Exception("Error Fetching Data");
     }
 
   }
@@ -60,32 +53,28 @@ class API{
   Future<List<FlSpot>> fetchConfirmedDeathData(String countryName)async
   {
 
-    try{
-      http.Response response = await http.get(Uri.parse("https://api.covid19api.com/total/dayone/country/"+countryName+"/status/deaths"));
+    http.Response response = await http.get(Uri.parse("https://api.covid19api.com/total/dayone/country/"+countryName+"/status/deaths"));
 
-      if(response.statusCode == 200){
+    if(response.statusCode == 200){
 
-        List<FlSpot> listOfSpots = <FlSpot>[];
+      List<FlSpot> listOfSpots = <FlSpot>[];
 
-        int count = 0;
+      int count = 0;
 
-        json.decode(response.body).forEach((element){
-          GraphData data = GraphData.fromJson(element);
-          int temp = data.count;
-          data.count -= count;
-          if(data.count < 0){
-            data.count = 0;
-          }
-          count = temp;
-          listOfSpots.add(FlSpot(data.date.millisecondsSinceEpoch.toDouble(), data.count.toDouble()));
-        });
+      json.decode(response.body).forEach((element){
+        GraphData data = GraphData.fromJson(element);
+        int temp = data.count;
+        data.count -= count;
+        if(data.count < 0){
+          data.count = 0;
+        }
+        count = temp;
+        listOfSpots.add(FlSpot(data.date.millisecondsSinceEpoch.toDouble(), data.count.toDouble()));
+      });
 
-        return listOfSpots;
+      return listOfSpots;
 
-      }else{
-        throw Exception();
-      }
-    }catch(e){
+    }else{
       throw Exception("Error Fetching Data");
     }
 
